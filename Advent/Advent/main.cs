bea981b730b33b777fb9f665d65aee971390b07a
Dan <dbulman68@gmail.com>
2023-01-09 01:27:58 +0000

    Completed Day 1

---
+++ Advent/Advent/main.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Day1;
+
+string puzzleInput = File.ReadAllText("puzzleInput.txt");
+part1.solve(puzzleInput);
+part2.solve(puzzleInput);
---
+++ Advent/Advent/part2.cs
@@ -0,0 +1,35 @@
+﻿/*Now, given the same instructions, find the position of the first character that causes him to enter the basement (floor -1). The first character in the instructions has position 1, the second character has position 2, and so on.
+
+For example:
+
+) causes him to enter the basement at character position 1.
+()()) causes him to enter the basement at character position 5.
+What is the position of the character that causes Santa to first enter the basement?*/
+namespace Day1
+{
+    internal static class part2
+    {
+        internal static void solve(string puzzleData) 
+        {
+            int floor = 0;
+            int firstNegative=0;
+            for(int i=0;i<puzzleData.Length;i++) 
+            {
+                if (puzzleData[i] == '(')
+                {
+                    floor++;
+                }
+                else if (puzzleData[i] == ')')
+                {
+                    floor--;
+                }
+                if(floor == -1) 
+                {
+                    firstNegative = i + 1; 
+                    break;
+                }
+            }
+            Console.WriteLine("The fist negative floor occurs at instruction position: " + firstNegative);
+        }
+    }
+}
