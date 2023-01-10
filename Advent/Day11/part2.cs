36d727441586813150c9fe5f89668e637d69ad86
Dan <dbulman68@gmail.com>
2023-01-10 09:16:44 +0000

    started day 11

---
+++ Advent/Day11/part2.cs
@@ -0,0 +1,15 @@
+﻿/**/
+using System.Diagnostics;
+
+namespace Day11
+{
+    internal static class part2
+    {
+        internal static void solve(string puzzleData)
+        {
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            watch.Stop();
+        }
+    }
+}
