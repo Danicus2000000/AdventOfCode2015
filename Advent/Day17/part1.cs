1a666a3d61f50b090b035e2c09394bff2c5271a0
Danicus2000000 <66948931+Danicus2000000@users.noreply.github.com>
2023-01-12 20:24:58 +0000

    Completed Day 17

---
+++ Advent/Day17/part1.cs
@@ -0,0 +1,46 @@
+﻿/*The elves bought too much eggnog again - 150 liters this time. To fit it all into your refrigerator, you'll need to move it into smaller containers. You take an inventory of the capacities of the available containers.
+
+For example, suppose you have containers of size 20, 15, 10, 5, and 5 liters. If you need to store 25 liters, there are four ways to do it:
+
+15 and 10
+20 and 5 (the first 5)
+20 and 5 (the second 5)
+15, 5, and 5
+Filling all containers entirely, how many different combinations of containers can exactly fit all 150 liters of eggnog?*/
+using System.Diagnostics;
+namespace Day17
+{
+    internal static class part1
+    {
+        internal static void solve(string puzzleData)
+        {
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            int target = 150;
+            int count = 0;
+            List<int> containers = new List<int>();
+            foreach (string line in puzzleData.Split("\r\n"))
+            {
+                containers.Add(int.Parse(line));
+            }
+
+            for (int i = 1; i <= Math.Pow(2, containers.Count); i++)
+            {
+                List<int> chosen = new List<int>();
+                for (int j = 0; j < containers.Count; j++)
+                {
+                    if ((i & (1 << j)) != 0)
+                    {
+                        chosen.Add(containers[j]);
+                    }
+                }
+                if (chosen.Sum(c => c) == target)
+                {
+                    count++;
+                }
+            }
+            watch.Stop();
+            Console.WriteLine("The number of ways to store the 150 liters is {0}. Calculated in {1}ms",count,watch.ElapsedMilliseconds);
+        }
+    }
+}
---
+++ Advent/Day17/part2.cs
@@ -0,0 +1,48 @@
+﻿/*While playing with all the containers in the kitchen, another load of eggnog arrives! The shipping and receiving department is requesting as many containers as you can spare.
+
+Find the minimum number of containers that can exactly fit all 150 liters of eggnog. How many different ways can you fill that number of containers and still hold exactly 150 litres?
+
+In the example above, the minimum number of containers was two. There were three ways to use that many containers, and so the answer there would be 3.*/
+using System.Diagnostics;
+namespace Day17
+{
+    internal static class part2
+    {
+        internal static void solve(string puzzleData)
+        {
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            int target = 150;
+            int count = 0;
+            int minContainer = int.MaxValue;
+            List<int> containers = new List<int>();
+            foreach (string line in puzzleData.Split("\r\n"))
+            {
+                containers.Add(int.Parse(line));
+            }
+
+            for (int i = 1; i <= Math.Pow(2, containers.Count); i++)
+            {
+                List<int> chosen = new List<int>();
+                for (int j = 0; j < containers.Count; j++)
+                {
+                    if ((i & (1 << j)) != 0)
+                    {
+                        chosen.Add(containers[j]);
+                    }
+                }
+                if(chosen.Sum(c => c) == target && chosen.Count()<minContainer) 
+                {
+                    count = 0;
+                    minContainer = chosen.Count();
+                }
+                if (chosen.Sum(c => c) == target && minContainer==chosen.Count())
+                {
+                    count++;
+                }
+            }
+            watch.Stop();
+            Console.WriteLine("The minimum number of containers to store 150 liters is {0},The number of ways to store the 150 liters that way is {1}. Calculated in {2}ms",minContainer, count, watch.ElapsedMilliseconds);
+        }
+    }
+}
