67628d975d420d4c30fabe70ca6e27d2ff079413
Danicus2000000 <66948931+Danicus2000000@users.noreply.github.com>
2023-01-12 00:31:09 +0000

    continuing day 14

---
+++ Advent/Day14/part1.cs
@@ -0,0 +1,59 @@
+﻿/*This year is the Reindeer Olympics! Reindeer can fly at high speeds, but must rest occasionally to recover their energy. Santa would like to know which of his reindeer is fastest, and so he has them race.
+
+Reindeer can only either be flying (always at their top speed) or resting (not moving at all), and always spend whole seconds in either state.
+
+For example, suppose you have the following Reindeer:
+
+Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.
+Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.
+After one second, Comet has gone 14 km, while Dancer has gone 16 km. After ten seconds, Comet has gone 140 km, while Dancer has gone 160 km. On the eleventh second, Comet begins resting (staying at 140 km), and Dancer continues on for a total distance of 176 km. On the 12th second, both reindeer are resting. They continue to rest until the 138th second, when Comet flies for another ten seconds. On the 174th second, Dancer flies for another 11 seconds.
+
+In this example, after the 1000th second, both reindeer are resting, and Comet is in the lead at 1120 km (poor Dancer has only gotten 1056 km by that point). So, in this situation, Comet would win (if the race ended at 1000 seconds).
+
+Given the descriptions of each reindeer (in your puzzle input), after exactly 2503 seconds, what distance has the winning reindeer traveled?*/
+using System.Diagnostics;
+
+namespace Day14
+{
+    public class Reindeer
+    {
+        public string Name { get; set; }
+        public int Speed { get; set; }
+        public int FlyTime { get; set; }
+        public int RestTime { get; set; }
+    }
+    internal static class part1
+    {
+        internal static void solve(string puzzleData)
+        {
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            Reindeer[] reindeer = new Reindeer[] {
+            new Reindeer { Name = "Comet", Speed = 14, FlyTime = 10, RestTime = 127 },
+            new Reindeer { Name = "Dancer", Speed = 16, FlyTime = 11, RestTime = 162 }
+        };
+
+            int raceTime = 2503;
+            int maxDistance = 0;
+
+            // find distance traveled for each reindeer
+            foreach (var r in reindeer)
+            {
+                int distance = 0;
+                int time = 0;
+                while (time < raceTime)
+                {
+                    int flyDistance = r.Speed * Math.Min(r.FlyTime, raceTime - time);
+                    distance += flyDistance;
+                    time += r.FlyTime;
+
+                    int restDistance = 0;
+                    time += r.RestTime;
+                }
+                maxDistance = Math.Max(maxDistance, distance);
+            }
+            watch.Stop();
+            Console.WriteLine("The winning reindeer traveled a distance of {0} km.", maxDistance);
+        }
+    }
+}
---
+++ Advent/Day14/part2.cs
@@ -0,0 +1,73 @@
+﻿/*Seeing how reindeer move in bursts, Santa decides he's not pleased with the old scoring system.
+
+Instead, at the end of each second, he awards one point to the reindeer currently in the lead. (If there are multiple reindeer tied for the lead, they each get one point.) He keeps the traditional 2503 second time limit, of course, as doing otherwise would be entirely ridiculous.
+
+Given the example reindeer from above, after the first second, Dancer is in the lead and gets one point. He stays in the lead until several seconds into Comet's second burst: after the 140th second, Comet pulls into the lead and gets his first point. Of course, since Dancer had been in the lead for the 139 seconds before that, he has accumulated 139 points by the 140th second.
+
+After the 1000th second, Dancer has accumulated 689 points, while poor Comet, our old champion, only has 312. So, with the new scoring system, Dancer would win (if the race ended at 1000 seconds).
+
+Again given the descriptions of each reindeer (in your puzzle input), after exactly 2503 seconds, how many points does the winning reindeer have?*/
+using System.Diagnostics;
+
+namespace Day14
+{
+    internal static class part2
+    {
+        public class Reindeer
+        {
+            public string Name { get; set; }
+            public int Speed { get; set; }
+            public int FlyTime { get; set; }
+            public int RestTime { get; set; }
+            public int Points { get; set; } // added property to hold the points
+            public int Distance { get; set; }
+            public int Remaining { get; set; }
+            public int totalTime { get; set; }
+            public int state { get; set; }
+            public int FlyCycle { get; set; }
+        }
+        internal static void solve(string puzzleData)
+        {
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            Reindeer[] reindeer = new Reindeer[] {
+            new Reindeer { Name = "Comet", Speed = 14, FlyTime = 10, RestTime = 127 },
+            new Reindeer { Name = "Dancer", Speed = 16, FlyTime = 11, RestTime = 162 }
+        };
+
+            int raceTime = 2503;
+
+            // simulate the race
+            for (int i = 1; i <= raceTime; i++)
+            {
+                // update the distance and state for each reindeer
+                foreach (var r in reindeer)
+                {
+                    if (r.state < r.FlyTime)
+                    {
+                        r.Distance += r.Speed;
+                        r.state++;
+                    }
+                    else if (r.state == r.FlyTime + r.RestTime)
+                    {
+                        r.state = 0;
+                        r.FlyCycle++;
+                    }
+                    else
+                    {
+                        r.state++;
+                    }
+                    if (r.FlyCycle > 0)
+                    {
+                        r.Points += r.FlyCycle;
+                    }
+                }
+                var max = reindeer.OrderByDescending(x => x.Distance).First();
+                max.Points++;
+            }
+            int maxPoints = reindeer.Max(r => r.Points);
+            watch.Stop();
+            Console.WriteLine("The winning reindeer has {0} points.", maxPoints);
+        }
+    }
+}
