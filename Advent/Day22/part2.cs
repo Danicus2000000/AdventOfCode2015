0c7212609ab7b6db38f2b965a1517b16e188a0f8
Dan <dbulman68@gmail.com>
2023-01-13 19:36:15 +0000

    Started Day 22

---
+++ Advent/Day22/part2.cs
@@ -0,0 +1,15 @@
+﻿/**/
+using System.Diagnostics;
+
+namespace Day22
+{
+    internal static class part2
+    {
+        internal static void solve(string puzzleData)
+        {
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            watch.Stop();
+        }
+    }
+}
